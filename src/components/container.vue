f7f414e9cec5e8dc5a389c97425ddad9c51ddf11
Hernani <hernani.luiz@gmail.com>
2021-08-22 17:43:56 -0300

    container

--- src/components/container.vue
+++ src/components/container.vue
@@ -1,18 +1,12 @@
 <template>
   <main class="container">
-    <navigation />
     <slot />
   </main>
 </template>
 
 <script>
-import Navigation from "./navigation";
 export default {
   name: "container",
-
-  components: {
-    Navigation,
-  },
 };
 </script>
 
@@ -24,6 +18,5 @@ export default {
   margin: auto;
   overflow: hidden;
   position: relative;
-  height: 100vw;
 }
 </style>
