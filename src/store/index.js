1af622bfe6ec270d5b3f7a6078aced6e8fa2fc83
Hernani <hernani.luiz@gmail.com>
2021-08-29 21:31:47 -0300

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,47 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { mapContext } from '../provider/MapProvider';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  strict: true,
   modules: {
-  }
-})
+    maps: {
+      namespaced: true,
+      state: {
+        mapsResult: [],
+        currentCoords: {
+          lat: 0,
+          lng: 0,
+        },
+      },
+      mutations: {
+        setCurrentLocation(state, location) {
+          state.currentCoords = { ...location };
+        },
+        setMapsResult(state, { results }) {
+          state.mapsResult = results;
+        },
+      },
+      actions: {
+        // eslint-disable-next-line no-unused-vars
+        async getMapsClosest({ commit }, _) {
+          const { coords } = await mapContext.getDataNavigator();
+          const { latitude, longitude } = coords;
+          const location = `${latitude}, ${longitude}`;
+          const { data } = await mapContext.getClosestsStores({ location });
+
+          commit('setMapsResult', data);
+        },
+
+        // eslint-disable-next-line no-unused-vars
+        async getCurrentCoords({ commit }, _) {
+          const { coords } = await mapContext.getDataNavigator();
+          const { latitude: lat, longitude: lng } = coords;
+          commit('setCurrentLocation', { lat, lng });
+        },
+      },
+    },
+  },
+});
